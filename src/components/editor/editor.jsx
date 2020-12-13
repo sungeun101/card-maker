45de3145f4cb2204ffdeb036bfef8f3bc5107984
sungeun101 <josungeun1019@gmail.com>
2020-12-13 22:22:24 +0900

    add card and edit form component

--- src/components/editor/editor.jsx
+++ src/components/editor/editor.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import Card from '../card/card';
+import CardEditForm from '../card_edit_form/card_edit_form';
 import styles from './editor.module.css'
 
-
-const Editor = () => {
+const Editor = ({ cards }) => {
     return (
         <section className={styles.editor}>
             <h1 className={styles.title}>Editor</h1>
+            {cards.map(card => <CardEditForm card={card} />)}
         </section>
     );
 };
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './maker.module.css'
 import Header from '../header/header';
 import Footer from '../footer/footer';
@@ -7,6 +7,42 @@ import Editor from '../editor/editor';
 import Preview from '../preview/preview';
 
 const Maker = ({ authService }) => {
+    const [cards, setCards] = useState([
+        {
+            id: '1',
+            name: 'ellie',
+            company: 'Samsung',
+            theme: 'colorful',
+            title: 'Software Engineer',
+            email: 'ellie@gmail.com',
+            message: 'go for it',
+            fileName: 'ellie',
+            fileURL: null
+        },
+        {
+            id: '2',
+            name: 'ellie2',
+            company: 'Samsung',
+            theme: 'dark',
+            title: 'Software Engineer',
+            email: 'ellie@gmail.com',
+            message: 'go for it',
+            fileName: 'ellie',
+            fileURL: 'ellie.png'
+        },
+        {
+            id: '3',
+            name: 'ellie3',
+            company: 'Samsung',
+            theme: 'light',
+            title: 'Software Engineer',
+            email: 'ellie@gmail.com',
+            message: 'go for it',
+            fileName: 'ellie',
+            fileURL: null
+        }
+    ]);
+
     const history = useHistory();
 
     const onLogout = () => {
@@ -25,8 +61,8 @@ const Maker = ({ authService }) => {
         <section className={styles.maker}>
             <Header onLogout={onLogout} />
             <div className={styles.container}>
-                <Editor />
-                <Preview />
+                <Editor cards={cards} />
+                <Preview cards={cards} />
             </div>
             <Footer />
         </section>
